aad8397d9151901b8efc12cd7dc9e596ed9d8136
dalavaiyuvaraj <dalavai.y@northeastern.edu>
2025-03-25 15:44:17 -0500

    Intial commit - Spring Boot Setup

---
+++ src/main/java/Github/Projects/Backend/TaskManagerApiApplication.java
@@ -0,0 +1,13 @@
+package Github.Projects.Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskManagerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Github/Projects/Backend/TaskManagerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package Github.Projects.Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
